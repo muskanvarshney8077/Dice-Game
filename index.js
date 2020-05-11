e746f43c9dec32d205fdd89a8e862cb6ba259888
muskanvarshney8077 <muskanvarshney8077@gmail.com>
2020-05-11 06:48:18 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomImage1="images/dice"+randomNumber1+".png";
+var images1=document.querySelectorAll('img')[0];
+images1.setAttribute('src',randomImage1);
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomImage2="images/dice"+randomNumber2+".png";
+var images2=document.querySelectorAll('img')[1];
+images2.setAttribute('src',randomImage2);
+
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML="player 1 wins";
+}
+else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML="player 2 wins";
+}
+else {
+  document.querySelector("h1").innerHTML="Tie";
+}
